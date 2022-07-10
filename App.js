ae01e089110f1b0a09c573cf5f8e6be1c614520b
Gilberto Martins <gilbertomartins643@gmail.com>
2022-07-09 21:53:11 -0300

    feat: Craindo interfaçe e script de validação(não funcional)

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput , TouchableOpacity} from 'react-native';
+import Validacao from './js/cpfValidacao.js'
+
 
 export default function App() {
+  const [cpf, setCpf] = useState('70223425478')
+
+  function validacao(valida) {
+    if(valida > 11){
+      return 'Verdadeiro'
+    }else{
+      return 'Falso'
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Valiação de CPF:</Text>
+
+      <TextInput style={[styles.imput, styles.text]}
+        placeholder={'CPF:'}
+        value={cpf}
+        onChangeText={text => setCpf(text)}
+        keyboardType={'numeric'}
+      >
+      </TextInput>
+
+      <TouchableOpacity  style={styles.resposta}>
+        <Text>Verificar CPF</Text>
+      </TouchableOpacity>
+
+
     </View>
+
   );
 }
 
@@ -18,4 +45,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imput: {
+    width: 250,
+    height: 44,
+    borderRadius: 15,
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: '#e8e8e8',
+    textAlign: 'center',
+  },
+  resposta: {
+    marginTop: 20,
+    padding: 5,
+    backgroundColor: '#e8e8e8',
+    width: 110,
+    height: 40,
+    borderRadius: 15,
+    alignItems: 'center',
+  }
 });
